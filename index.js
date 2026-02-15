c116b7baccb6c722d7f2bf5c02387769fb138131
Md Fuad Amir <mdfuadamir@gmail.com>
2026-02-15 17:08:07 +0600

    added bug fixed

--- index.js
+++ index.js
@@ -1,84 +1,4 @@
-// //===================== dependencies import ====================//
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// dotenv.config();
-
-// const Products = require("./src/Models/Products");
-// const Profile = require("./src/Models/Profile");
-// const Cart = require("./src/Models/Cart");
-// const wishlist = require("./src/Models/wishlist");
-// const Order = require("./src/Models/Order");
-// const AdminStats = require("./src/Models/AdminStats");
-// const UserStats = require("./src/Models/UserStats");
-// const Contact = require("./src/Models/Contact");
-// const Users = require("./src/Models/Users");
-// const feedbacks = require("./src/Models/feedbacks");
-// const Review = require("./src/Models/Review");
-// //===================== express app setup ====================//
-// const app = express();
-// const port = process.env.PORT || 3000;
-// //===================== middleware ====================//
-
-// // MIDDLEWARE
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   }),
-// );
-// app.options("*", cors());
-// app.use(express.json());
-// //===================== FIREBASE MIDDLEWARE =========================//
-
-// //===================== MongoDB connection string ====================//
-// const uri = `mongodb+srv://${process?.env?.DB_USER}:${process?.env?.DB_PASS}@cluster0.sldyvva.mongodb.net/?appName=Cluster0`;
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-// //======================== main run function =======================//
-// async function run() {
-//   try {
-//     // await client.connect();
-
-//     Users(app, client);
-//     Profile(app, client);
-//     Products(app, client);
-//     Cart(app, client);
-//     wishlist(app, client);
-//     Order(app, client)
-//     AdminStats(app, client)
-//     UserStats(app, client)
-//     Contact(app, client)
-//     feedbacks(app, client)
-//     Review(app, client)
-//     //==================== MongoDB connection test =================//
-//     // await client.db("admin").command({ ping: 1 });
-//     // console.log(
-//     //   "Pinged your deployment. You successfully connected to MongoDB!",
-//     // );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-// //========================= simple root route ========================//
-// app.get("/", (req, res) => {
-//   res.send("MMAW server is running");
-// });
-// //================= Start server =================//
-// app.listen(port, () => {
-//   console.log(`server is running on port ${port}`);
-// });
-
-
-
+//===================== dependencies import ====================//
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -96,17 +16,23 @@ const Contact = require("./src/Models/Contact");
 const Users = require("./src/Models/Users");
 const feedbacks = require("./src/Models/feedbacks");
 const Review = require("./src/Models/Review");
-
+//===================== express app setup ====================//
 const app = express();
+const port = process.env.PORT || 3000;
+//===================== middleware ====================//
 
-app.use(cors({
-  origin: ["http://localhost:5173"],
-  credentials: true,
-}));
+// MIDDLEWARE
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
+//===================== FIREBASE MIDDLEWARE =========================//
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sldyvva.mongodb.net/?appName=Cluster0`;
-
+//===================== MongoDB connection string ====================//
+const uri = `mongodb+srv://${process?.env?.DB_USER}:${process?.env?.DB_PASS}@cluster0.sldyvva.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -114,32 +40,39 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
-let isConnected = false;
-
+//======================== main run function =======================//
 async function run() {
-  if (!isConnected) {
-    await client.connect();
-    isConnected = true;
+  try {
+    // await client.connect();
+
+    Users(app, client);
+    Profile(app, client);
+    Products(app, client);
+    Cart(app, client);
+    wishlist(app, client);
+    Order(app, client)
+    AdminStats(app, client)
+    UserStats(app, client)
+    Contact(app, client)
+    feedbacks(app, client)
+    Review(app, client)
+    //==================== MongoDB connection test =================//
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!",
+    // );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
-
-  Users(app, client);
-  Profile(app, client);
-  Products(app, client);
-  Cart(app, client);
-  wishlist(app, client);
-  Order(app, client);
-  AdminStats(app, client);
-  UserStats(app, client);
-  Contact(app, client);
-  feedbacks(app, client);
-  Review(app, client);
 }
-
 run().catch(console.dir);
-
+//========================= simple root route ========================//
 app.get("/", (req, res) => {
   res.send("MMAW server is running");
 });
+//================= Start server =================//
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
 
-module.exports = app;
